7b9e72777002303c84ced61cd9e7561a196a511c
= <=>
2026-07-12 08:16:58 +0200

    for running faster walker launcher

--- hypr/modules/auto_start.lua
+++ hypr/modules/auto_start.lua
@@ -6,7 +6,7 @@ local M = {}
 function M.setup()
 	print("Auto Start Module Loaded")
 	hl.on("hyprland.start", function()
-		hl.exec_cmd("elephant & waybar")
+		hl.exec_cmd("elephant & walker --gapplication-service & waybar")
 	end)
 end
 
