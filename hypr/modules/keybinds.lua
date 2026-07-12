fc7adf7443f7d81aba0ddd521881fb2faa6e7b0f
= <=>
2026-07-12 20:26:15 +0200

    keybinds

---
+++ hypr/modules/keybinds.lua
@@ -0,0 +1,13 @@
+-- Keybinds Module
+-- See https://wiki.hypr.land/Configuring/Basics/Autostart/
+
+local M = {}
+
+function M.setup()
+	print("Auto Start Module Loaded")
+	hl.on("hyprland.start", function()
+		hl.exec_cmd("elephant & walker --gapplication-service & aww-daemon & waybar")
+	end)
+end
+
+return M
