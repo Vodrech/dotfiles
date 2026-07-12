c06d7762b59cbc1827f7194470141190a468d2c8
= <=>
2026-07-12 06:36:41 +0200

    print message

--- hypr/hyprland.lua
+++ hypr/hyprland.lua
@@ -1,13 +1,4 @@
--- This is an example Hyprland Lua config file.
--- Refer to the wiki for more information.
--- https://wiki.hypr.land/Configuring/Start/
-
--- Please note not all available settings / options are set here.
--- For a full list, see the wiki
-
--- You can (and should!!) split this configuration into multiple files
--- Create your files separately and then require them like this:
--- require("myColors")
+print("Hyprland Lua Configuration Loaded")
 
 -- Loading the Modules
 local monitor = require("modules.monitor")
