ae3c207a73a8c2c34d723d1502ff201280cbc759
= <=>
2026-07-12 07:40:41 +0200

    monitor fix

--- hypr/modules/monitor.lua
+++ hypr/modules/monitor.lua
@@ -5,10 +5,22 @@ local M = {}
 
 function M.setup()
 	hl.monitor({
-		output = "",
-		mode = "",
-		position = "",
-		scale = "auto",
+		output = "DP-2",
+		mode = "1920x1080@60",
+		position = "0x0",
+		scale = "1",
+	})
+	hl.monitor({
+		output = "HDMI-A-1",
+		mode = "2560x1440@144@144",
+		position = "1920x0",
+		scale = "1",
+	})
+	hl.monitor({
+		output = "HDMI-A-2",
+		mode = "preferred",
+		position = "1920x1440",
+		scale = "1",
 	})
 end
 
