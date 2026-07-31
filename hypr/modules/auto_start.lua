d934d272f9054958d8c61bb52d4f2050d56ec8fe
= <=>
2026-08-01 00:55:27 +0200

    added quickshell as mainbar now

--- hypr/modules/auto_start.lua
+++ hypr/modules/auto_start.lua
@@ -6,7 +6,7 @@ local M = {}
 function M.setup()
 	print("Auto Start Module Loaded")
 	hl.on("hyprland.start", function()
-		hl.exec_cmd("elephant & walker --gapplication-service & waybar")
+		hl.exec_cmd("elephant & walker --gapplication-service & quickshell")
 		hl.exec_cmd("sleep 1 && awww-daemon")
 	end)
 end
