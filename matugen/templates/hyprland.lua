3e832a90f342bf6b83e38e81984978e1d871ab4f
= <=>
2026-08-01 02:37:46 +0200

    thmee fixes

--- matugen/templates/hyprland.lua
+++ matugen/templates/hyprland.lua
@@ -1,9 +1,8 @@
--- Matugen Hyprland color palette
 return {
-    primary = "rgba({{colors.primary.default.hex}}ff)",
-    secondary = "rgba({{colors.secondary.default.hex}}ff)",
-    tertiary = "rgba({{colors.tertiary.default.hex}}ff)",
-    surface_variant = "rgba({{colors.surface_variant.default.hex}}ff)",
-    background = "rgba({{colors.background.default.hex}}ff)",
-    foreground = "rgba({{colors.on_background.default.hex}}ff)",
+    primary = "rgba({{colors.primary.default.hex_stripped}}ff)",
+    secondary = "rgba({{colors.secondary.default.hex_stripped}}ff)",
+    tertiary = "rgba({{colors.tertiary.default.hex_stripped}}ff)",
+    surface_variant = "rgba({{colors.surface_variant.default.hex_stripped}}ff)",
+    background = "rgba({{colors.background.default.hex_stripped}}ff)",
+    foreground = "rgba({{colors.on_background.default.hex_stripped}}ff)",
 }
