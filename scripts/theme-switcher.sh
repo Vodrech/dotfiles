5eb64dfd6ce4af1dfbf8293715b8bd82640e7fb8
= <=>
2026-08-01 03:14:41 +0200

    awaiting for it to load properly

--- scripts/theme-switcher.sh
+++ scripts/theme-switcher.sh
@@ -1,25 +1,22 @@
 #!/usr/bin/env bash
 
 THEME=$1
-
 WALLPAPER="$HOME/.assets/wallpapers/$THEME.jpg"
 
-# Change wallpaper
+# 1. Start the wallpaper transition (takes 1 second)
 awww img "$WALLPAPER" \
     --transition-type grow \
-    --transition-duration 1
+    --transition-duration 1 &
 
-# Generate colors
-matugen image "$WALLPAPER" \
-	  --source-color-index 0
+# 2. Run matugen and reloads concurrently in the background 
+# so they don't block the UI thread during the animation
+(
+    matugen image "$WALLPAPER" --source-color-index 0
+    hyprctl reload
+    pkill quickshell
+    quickshell &
+    sed -i "s/theme = .*/theme = \"$THEME\"/" ~/.config/walker/config.toml
+) &
 
-# Reload Hyprland
-hyprctl reload
-
-# Reload Waybar
-pkill quickshell
-quickshell &
-
-# Reload Walker theme
-sed -i "s/theme = .*/theme = \"$THEME\"/" \
-    ~/.config/walker/config.toml
+# Wait for all background tasks to cleanly wrap up
+wait
