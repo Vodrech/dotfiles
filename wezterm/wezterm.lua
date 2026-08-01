fbce3950497782bacf6c0fdc707cf5888a2cd90f
= <=>
2026-08-01 02:49:53 +0200

    wezterm colors

--- wezterm/wezterm.lua
+++ wezterm/wezterm.lua
@@ -1,9 +1,23 @@
--- Pull in the wezterm API
 local wezterm = require 'wezterm'
-
--- This will hold the configuration.
 local config = wezterm.config_builder()
 
-config.font = wezterm.font('FiraCode Nerd Font', {weight="Regular", stretch="Normal", style="Normal"})
+-- 1. Point to your WezTerm Matugen cache directory
+package.path = package.path .. ";/home/vodrech/.cache/matugen/wezterm/?.lua"
+
+-- 2. Load the dynamic colors
+local matugen_colors = require("colors")
+config.colors = matugen_colors
+
+-- 3. Glassmorphism Settings
+config.window_background_opacity = 0.85
+config.wayland_window_background_blur = true
+
+-- Optional window styling padding
+config.window_padding = {
+    left = '15pt',
+    right = '15pt',
+    top = '15pt',
+    bottom = '15pt',
+}
 
 return config
