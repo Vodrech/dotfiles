436e07423cf9cde498ce19214b3d3f8ea6bf0eb7
= <=>
2026-08-21 01:55:38 +0200

    curors edits

--- hypr/modules/environment.lua
+++ hypr/modules/environment.lua
@@ -3,8 +3,8 @@
 local M = {}
 
 function M.setup()
-	hl.env("XCURSOR_SIZE", "24")
-	hl.env("XCURSOR_THEME", "Bibata-Modern-Ice")
+	hl.env("HYPRCURSOR_SIZE", "24")
+	hl.env("HYPRCURSOR_THEME", "Bibata-Modern-Ice")
 end
 
 return M
